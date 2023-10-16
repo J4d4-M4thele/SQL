8ef949765fce07b61c6ba43ba1bd328429b6758f
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-16 14:36:00 +0200

    update

--- chapter07/Extra-Example.sql
+++ chapter07/Extra-Example.sql
@@ -26,7 +26,20 @@ both tables by id (as each song and album has a unique id)
 
 --Question 2
 '
-Another column that can be unique is the song_artist column as no two artists
+Another column in the albums table that can be unique is the album_catalog_code column as
+every album has its own unique code.
+So by using the album_id and album_catalog_code columns, one can create a natural key. 
+
+Another column in the songs table that can be unique is the song_artist column as no two artists
 have the same name (it\'s common practice to seek out a unique name).
 So by using the song_id and song_artist columns, one can create a natural key. 
+'
+
+--Question 3
+'
+In the albums table the best  column to allocate an index
+to is the album_genre column.
+
+In the songs table the best  column to allocate an index
+to is the song_title or song_artist columns.
 '
