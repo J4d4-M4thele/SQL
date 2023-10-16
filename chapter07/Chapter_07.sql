2c6a9f87f321c8b26385d1463240cd35604bce9e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-16 10:57:08 +0200

    update

--- chapter07/Chapter_07.sql
+++ chapter07/Chapter_07.sql
@@ -92,9 +92,28 @@ CREATE TABLE check_constraint_example (
     CONSTRAINT check_salary_not_zero CHECK (salary > 0)
 );
 
---listing 7.8(both have failed)
+--both will failed
 INSERT INTO check_constraint_example (user_role)
 VALUES ('admin');
 
 INSERT INTO check_constraint_example (salary)
 VALUES (0);
+
+--listing 7.8(unique constraint)
+
+CREATE TABLE unique_constraint_example (
+    contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(200),
+    CONSTRAINT email_unique UNIQUE (email)
+);
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Samantha', 'Lee', 'slee@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Betty', 'Diaz', 'bdiaz@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Sasha', 'Lee', 'slee@example.org');
