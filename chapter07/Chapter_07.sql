fe93d2da3d4528861b2318a521003f440a9b274e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-16 10:40:17 +0200

    update

--- chapter07/Chapter_07.sql
+++ chapter07/Chapter_07.sql
@@ -24,3 +24,12 @@ VALUES ('T229901', 'Lynn', 'Malero');
 
 INSERT INTO natural_key_example (license_id, first_name, last_name)
 VALUES ('T229901', 'Sam', 'Tracy');
+
+-- Listing 7-3(Declaring a composite primary key as a natural key)
+CREATE TABLE natural_key_composite_example (
+    student_id varchar(10),
+    school_day date,
+    present boolean,
+    CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
