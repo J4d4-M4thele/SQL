6fe7d1c5542c77e84117626ab94d47a7b67dd0d7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-16 14:30:26 +0200

    update

--- chapter07/Extra-Example.sql
+++ chapter07/Extra-Example.sql
@@ -1,5 +1,4 @@
 --Question 1
-
 CREATE TABLE albums (
 album_id bigserial,
 album_catalog_code varchar(100),
@@ -23,4 +22,11 @@ CONSTRAINT song_key PRIMARY KEY (album_id, song_id)
 I chose a foreign key because the albums id in the albums table
 is referenced in the songs table as well so it is best to link 
 both tables by id (as each song and album has a unique id)
+'
+
+--Question 2
+'
+Another column that can be unique is the song_artist column as no two artists
+have the same name (it\'s common practice to seek out a unique name).
+So by using the song_id and song_artist columns, one can create a natural key. 
 '
