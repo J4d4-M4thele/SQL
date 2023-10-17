e11cf9541c24da76a63841da10fdfb7c3653efa4
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-17 11:35:32 +0200

    update

--- chapter08/Chapter_08.sql
+++ chapter08/Chapter_08.sql
@@ -1,17 +1,77 @@
 --listing 8.1(creating and filling 2014 table)
 CREATE TABLE pls_fy2014_pupld14a (
-stabr varchar(2) NOT NULL,
-fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
-libid varchar(20) NOT NULL,
-libname varchar(100) NOT NULL,
-obereg varchar(2) NOT NULL,
-rstatus integer NOT NULL,
-statstru varchar(2) NOT NULL,
-statname varchar(2) NOT NULL,
-stataddr varchar(2) NOT NULL,
---snip--
-wifisess integer NOT NULL,
-yr_sub integer NOT NULL
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio_ph integer NOT NULL,
+    audio_dl float NOT NULL,
+    video_ph integer NOT NULL,
+    video_dl float NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    elmatcir integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    wifisess integer NOT NULL,
+    yr_sub integer NOT NULL
 );
 
 CREATE INDEX libname2014_idx ON pls_fy2014_pupld14a (libname);
@@ -25,18 +85,74 @@ WITH (FORMAT CSV, HEADER);
 
 --listing 8.2(creating and filling 2009 table)
 CREATE TABLE pls_fy2009_pupld09a (
-stabr varchar(2) NOT NULL,
-fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
-libid varchar(20) NOT NULL,
-libname varchar(100) NOT NULL,
-address varchar(35) NOT NULL,
-city varchar(20) NOT NULL,
-zip varchar(5) NOT NULL,
-zip4 varchar(4) NOT NULL,
-cnty varchar(20) NOT NULL,
---snip--
-fipsst varchar(2) NOT NULL,
-fipsco varchar(3) NOT NULL
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio integer NOT NULL,
+    video integer NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    yr_sub integer NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL
 );
 
 CREATE INDEX libname2009_idx ON pls_fy2009_pupld09a (libname);
@@ -94,3 +210,44 @@ SELECT stabr, stataddr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr, stataddr
 ORDER BY stabr ASC, count(*) DESC;
+
+--listing 8.11(Using the sum() aggregate function to total visits to libraries in 2014 and 2009)
+SELECT sum(visits) AS visits_2014
+FROM pls_fy2014_pupld14a
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2009
+FROM pls_fy2009_pupld09a
+WHERE visits >= 0;
+
+--listing 8.12(using sum() to total visits for both tables)
+SELECT sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0;
+
+--listing 8.13(percentage change)
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+--listing 8.14
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+HAVING sum(pls14.visits) > 50000000
+ORDER BY pct_change DESC;
