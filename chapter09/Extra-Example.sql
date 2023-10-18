1cbe69cdf03c8425305886de282ed68276c6ddd7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-18 14:23:54 +0200

    update

--- chapter09/Extra-Example.sql
+++ chapter09/Extra-Example.sql
@@ -1,3 +1,33 @@
 --Question 1
 ALTER TABLE meat_poultry_egg_inspect ADD COLUMN meat_processing boolean;
 ALTER TABLE meat_poultry_egg_inspect ADD COLUMN poultry_processing boolean;
+
+--Question 2
+UPDATE meat_poultry_egg_inspect inspect
+SET meat_processing = TRUE
+--SELECT * FROM meat_poultry_egg_inspect inspect
+WHERE activities LIKE '%Meat Processing%';
+
+UPDATE meat_poultry_egg_inspect inspect
+SET meat_processing = TRUE
+--SELECT * FROM meat_poultry_egg_inspect inspect
+WHERE activities LIKE '%Poultry Processing%';
+
+--Question 3
+--no. of co.s that process meat
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE meat_processing = true
+AND poultry_processing IS NULL;
+
+--no. of co.s that process poultry
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE poultry_processing = true
+AND meat_processing IS NULL;
+
+--no. of co.s that process poultry
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE meat_processing = true
+AND poultry_processing = true;
