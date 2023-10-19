451cc3e950e1073a0ab914cd3e41c31b863379ac
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-19 21:03:33 +0200

    update

--- chapter11/Extra-Examples.sql
+++ chapter11/Extra-Examples.sql
@@ -1,11 +1,10 @@
 --Question 1
-Using the New York City taxi data, calculate the length of each ride
-using the pickup and drop-off timestamps. Sort the query results from the
-longest ride to the shortest. Do you notice anything about the longest or
-shortest trips that you might want to ask city officials about?
-
-SELECT   tpep_pickup_datetime 
+SELECT vendor_id,
+    tpep_pickup_datetime, 
     tpep_dropoff_datetime
+   arrival - departure AS segment_time 
+  FROM nyc_yellow_taxi_trips_2016_06_01
+  ORDER BY segment_time
 --Question 2
 
 --Question 3
