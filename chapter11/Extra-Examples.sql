6c185631451718c28ab66d0c1c563c7c3e35de50
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-19 21:22:21 +0200

    update

--- chapter11/Extra-Examples.sql
+++ chapter11/Extra-Examples.sql
@@ -1,10 +1,16 @@
 --Question 1
 SELECT vendor_id,
     tpep_pickup_datetime, 
-    tpep_dropoff_datetime
-   arrival - departure AS segment_time 
-  FROM nyc_yellow_taxi_trips_2016_06_01
-  ORDER BY segment_time
+    tpep_dropoff_datetime,
+    tpep_pickup_datetime - tpep_dropoff_datetime AS segment_time
+FROM nyc_yellow_taxi_trips_2016_06_01
+ORDER BY (segment_time) DESC;
+
+'
+There are negative intervals where the dropoff time
+is before the pickup time. Meaning the trip ended
+before it began.
+'
 --Question 2
 
 --Question 3
