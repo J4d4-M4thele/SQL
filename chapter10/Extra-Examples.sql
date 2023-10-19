87ccd64860f882bb271f1b308c928b0ab7efd5e4
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-19 11:56:34 +0200

    update

--- chapter10/Extra-Examples.sql
+++ chapter10/Extra-Examples.sql
@@ -42,3 +42,16 @@ WHERE population >= 500000
 ORDER BY (motor_vehicle_theft::numeric / population) DESC;
 
 --Question 3
+SELECT pls14.stabr,
+city,
+libname,
+visits,
+sum(pls14.visits) AS visits_2014,
+round(
+    (visits::numeric/ sum(visits)) * 1000, 1
+) AS visits_per_1000
+FROM pls_fy2014_pupld14a
+WHERE visits >= 250000
+GROUP BY pls14.stabr
+ORDER BY (visits::numeric/ sum(visits)) DESC;
+
